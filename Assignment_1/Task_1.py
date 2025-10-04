5db88aa979be87770a75f34b2de6006f268ab0b4
SPandey001 <pandeyshubhanshu12@gmail.com>
2025-10-04 13:15:30 +0530

    Completed

---
+++ Assignment_1/Task_1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter First number: "))
+b = int(input("Enter Second number: "))
+
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
